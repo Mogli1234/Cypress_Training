c1ba730cedd193ad5e5f6fe716ad3f83377205fa
Jaime Esquivel <jaimeesquivelgomez@gmail.com>
2020-11-06 12:53:46 -0600

    modificado el stage 3 agregando los dos test en uno

---
+++ cypress/integration/demo/ejemplo.js
@@ -0,0 +1,26 @@
+it('Sing up Test',() => {
+    const imageUpload = '602006.jpg'
+    cy.visit('http://trainingqa.avantica.avanticatec.net:3000/')
+    cy.get('#Login_bto').click()
+    cy.wait(5000)
+    cy.get('.Signup-bto-page').click()
+    cy.get('#file').attachFile(imageUpload)
+    cy.get('#firstname').type('ejemploPrimerNombre')
+    cy.get('#lastname').type('ejemploApellidos')
+    cy.get('#email').type(getRandomString(8)+'@gmail.com')
+    cy.get('#username').type(getRandomString(8))
+    cy.get('#phone').type('84815552')
+    cy.get('#password').type('123456789')
+    cy.get('#confirm').type('123456789')
+    cy.get('.SignUp-bto').click()
+    cy.get('#Login_bto').should('be.visible').click()
+})
+
+function getRandomString(length) {
+    var randomChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var result = '';
+    for ( var i = 0; i < length; i++ ) {
+        result += randomChars.charAt(Math.floor(Math.random() * randomChars.length));
+    }
+    return result;
+}
--- cypress/integration/training_stage_scripts/stage03_sing_up_test.spec.js
+++ cypress/integration/training_stage_scripts/stage03_sing_up_test.spec.js
@@ -1,22 +1,35 @@
 /// <reference types="cypress" />
+describe('Training Cypress Tests',()=>{
 
-it('Sing up Test',() => {
-    const imageUpload = '602006.jpg'
-    cy.visit('http://trainingqa.avantica.avanticatec.net:3000/')
-    cy.get('#Login_bto').click()
-    cy.wait(5000)
-    cy.get('.Signup-bto-page').click()
-    cy.get('#file').attachFile(imageUpload)
-    cy.get('#firstname').type('ejemploPrimerNombre')
-    cy.get('#lastname').type('ejemploApellidos')
-    cy.get('#email').type(getRandomString(8)+'@gmail.com')
-    cy.get('#username').type(getRandomString(8))
-    cy.get('#phone').type('84815552')
-    cy.get('#password').type('123456789')
-    cy.get('#confirm').type('123456789')
-    cy.get('.SignUp-bto').click()
-    cy.get('#Login_bto').should('be.visible').click()
-})
+    it('Sing up Test',() => {
+        const imageUpload = '602006.jpg'
+        cy.visit('http://trainingqa.avantica.avanticatec.net:3000/')
+        cy.get('#Login_bto').click()
+        cy.wait(5000)
+        cy.get('.Signup-bto-page').click()
+        cy.get('#file').attachFile(imageUpload)
+        cy.get('#firstname').type('ejemploPrimerNombre')
+        cy.get('#lastname').type('ejemploApellidos')
+        cy.get('#email').type(getRandomString(8)+'@gmail.com')
+        cy.get('#username').type(getRandomString(8))
+        cy.get('#phone').type('84815552')
+        cy.get('#password').type('123456789')
+        cy.get('#confirm').type('123456789')
+        cy.get('.SignUp-bto').click()
+        cy.get('#Login_bto').should('be.visible').click()
+    })
+
+    it('Login_Test_Correctly',() =>{
+        cy.visit('http://trainingqa.avantica.avanticatec.net:3000/')
+        cy.wait(10000)
+        cy.get('#Login_bto').click()
+        cy.wait(5000)
+        cy.get('#email').type('jaime.esquivel@avantica.com')
+        cy.get('#password').type('123456789')
+        cy.get('.Login-bto').click()
+        cy.get('#Login_bto').contains('LOGOUT')
+    })
+});
 
 function getRandomString(length) {
     var randomChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
