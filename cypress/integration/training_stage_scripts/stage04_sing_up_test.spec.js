7b8a74e83b676a882e573015ede02a199f0d0404
Jaime Esquivel <jaimeesquivelgomez@gmail.com>
2020-11-11 12:34:19 -0600

    creacion del stage 4 file y los agregados del stage 4

--- cypress/integration/training_stage_scripts/stage04_sing_up_test.spec.js
+++ cypress/integration/training_stage_scripts/stage04_sing_up_test.spec.js
@@ -1,22 +1,34 @@
 /// <reference types="cypress" />
+describe('Training Cypress Tests for stage 4',()=>{
+    describe('Before Method to acces to the login page',()=>{
+        beforeEach(()=>{
+            cy.visit('http://trainingqa.avantica.avanticatec.net:3000/')
+            cy.get('#Login_bto').click()
+        })
+    })
 
-it('Sing up Test',() => {
-    const imageUpload = '602006.jpg'
-    cy.visit('http://trainingqa.avantica.avanticatec.net:3000/')
-    cy.get('#Login_bto').click()
-    cy.wait(5000)
-    cy.get('.Signup-bto-page').click()
-    cy.get('#file').attachFile(imageUpload)
-    cy.get('#firstname').type('ejemploPrimerNombre')
-    cy.get('#lastname').type('ejemploApellidos')
-    cy.get('#email').type(getRandomString(8)+'@gmail.com')
-    cy.get('#username').type(getRandomString(8))
-    cy.get('#phone').type('84815552')
-    cy.get('#password').type('123456789')
-    cy.get('#confirm').type('123456789')
-    cy.get('.SignUp-bto').click()
-    cy.get('#Login_bto').should('be.visible').click()
-})
+    it('Sing up Test',() => {
+        const imageUpload = '602006.jpg'
+        cy.get('.Signup-bto-page').click()
+        cy.get('#file').should("be.visible").and("be.enabled").attachFile(imageUpload)
+        cy.get('#firstname').should("be.visible").and("be.empty").type('ejemploPrimerNombre')
+        cy.get('#lastname').should("be.visible").and("be.empty").type('ejemploApellidos')
+        cy.get('#email').should("be.visible").and("be.empty").type(getRandomString(8)+'@gmail.com')
+        cy.get('#username').should("be.visible").and("be.empty").type(getRandomString(8))
+        cy.get('#phone').should("be.visible").and("be.empty").type('84815552')
+        cy.get('#password').should("be.visible").and("be.empty").type('123456789')
+        cy.get('#confirm').should("be.visible").and("be.empty").type('123456789')
+        cy.get('.SignUp-bto').should("be.visible").and("be.empty").click()
+        cy.get('#Login_bto').should("be.visible").and("be.empty").should('be.visible').click()
+    })
+
+    it('Login_Test_Correctly',() =>{
+        cy.get('#email').should("be.visible").and("be.empty").type('jaime.esquivel@avantica.com')
+        cy.get('#password').should("be.visible").and("be.empty").type('123456789')
+        cy.get('.Login-bto').click()
+        cy.get('#Login_bto').contains('LOGOUT')
+    })
+});
 
 function getRandomString(length) {
     var randomChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
