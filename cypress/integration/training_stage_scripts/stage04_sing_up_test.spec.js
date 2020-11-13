5831bfe98a7e3f9dfcab737d5943328aa1638349
Jaime Esquivel <jaimeesquivelgomez@gmail.com>
2020-11-13 10:41:51 -0600

    arreglando issue

--- cypress/integration/training_stage_scripts/stage04_sing_up_test.spec.js
+++ cypress/integration/training_stage_scripts/stage04_sing_up_test.spec.js
@@ -1,15 +1,10 @@
 /// <reference types="cypress" />
 describe('Training Cypress Tests for stage 4',()=>{
-    describe('Before Method to acces to the login page',()=>{
-        beforeEach(()=>{
-            cy.visit('http://trainingqa.avantica.avanticatec.net:3000/')
-            cy.get('#Login_bto').click()
-        })
-    })
-
     it('Sing up Test',() => {
-        const imageUpload = '602006.jpg'
+        cy.visit('http://trainingqa.avantica.avanticatec.net:3000/login')
+        cy.wait(5000)
         cy.get('.Signup-bto-page').click()
+        const imageUpload = '602006.jpg'
         cy.get('#file').should("be.visible").and("be.enabled").attachFile(imageUpload)
         cy.get('#firstname').should("be.visible").and("be.empty").type('ejemploPrimerNombre')
         cy.get('#lastname').should("be.visible").and("be.empty").type('ejemploApellidos')
@@ -18,11 +13,12 @@ describe('Training Cypress Tests for stage 4',()=>{
         cy.get('#phone').should("be.visible").and("be.empty").type('84815552')
         cy.get('#password').should("be.visible").and("be.empty").type('123456789')
         cy.get('#confirm').should("be.visible").and("be.empty").type('123456789')
-        cy.get('.SignUp-bto').should("be.visible").and("be.empty").click()
-        cy.get('#Login_bto').should("be.visible").and("be.empty").should('be.visible').click()
+        cy.get('.SignUp-bto').click()
+        cy.get('#Login_bto').should('be.visible').click()
     })
 
     it('Login_Test_Correctly',() =>{
+        cy.visit('http://trainingqa.avantica.avanticatec.net:3000/login')
         cy.get('#email').should("be.visible").and("be.empty").type('jaime.esquivel@avantica.com')
         cy.get('#password').should("be.visible").and("be.empty").type('123456789')
         cy.get('.Login-bto').click()
