5039f032f76b4c582e3c2f8ddbe9fa8d9211727b
Jaime Esquivel <jaimeesquivelgomez@gmail.com>
2020-11-20 12:32:42 -0600

    agregado un nuevo script para el stage 06

---
+++ cypress/integration/training_stage_scripts/stage06_custom_commands.spec.js
@@ -0,0 +1,38 @@
+/// <reference types="cypress" />
+
+import * as helpF from './Helpers/randomString';
+
+describe('Training Cypress Tests for stage 4',()=>{
+    beforeEach(() => {
+        cy.visit('http://trainingqa.avantica.avanticatec.net:3000/login')
+    })
+
+    before(function(){
+        cy.fixture('loginData').then(function(loginData){
+            this.loginData  = loginData;
+        })
+    })
+
+    it('Login_Test_Correctly',function(){
+        cy.get('#email').type(this.loginData.email)
+        cy.get('#password').type(this.loginData.password)
+        cy.get('.Login-bto').click()
+        cy.get('#Login_bto').should('be.visible').contains('LOGOUT')
+    })
+
+    it('Sing up Test',() => {
+        cy.get('.Signup-bto-page').click()
+        const imageUpload = '602006.jpg'
+        cy.get('#file').attachFile(imageUpload)
+        cy.get('#firstname').type('ejemploPrimerNombre')
+        cy.get('#lastname').type('ejemploApellidos')
+        cy.get('#email').type(helpF.getRandomString(8)+'@gmail.com')
+        cy.get('#username').type(helpF.getRandomString(8))
+        cy.get('#phone').type('84815552')
+        cy.get('#password').type('123456789')
+        cy.get('#confirm').type('123456789')
+        cy.get('.SignUp-bto').click()
+        cy.get('#Login_bto').should('be.visible').contains('LOGOUT')
+    })
+});
+
