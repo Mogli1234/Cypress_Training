2773e38d30af107d07f8d7355f92000cf4794b9e
Jaime Esquivel <jaimeesquivelgomez@gmail.com>
2020-11-24 13:47:49 -0600

    modificado el stage 05 con las mejoras

--- cypress/integration/training_stage_scripts/stage05_fixtures.spec.js
+++ cypress/integration/training_stage_scripts/stage05_fixtures.spec.js
@@ -2,25 +2,37 @@
 
 import * as helpF from './Helpers/randomString';
 
-describe('Training Cypress Tests for stage 4',()=>{
-    beforeEach(() => {
+describe('Training Cypress Tests for stage 4', () => {
+
+    beforeEach( () => {
         cy.visit('http://trainingqa.avantica.avanticatec.net:3000/login')
+        cy.fixture('loginData').as('loginData')
     })
 
-    before(function(){
-        cy.fixture('loginData').then(function(loginData){
-            this.loginData  = loginData;
-        })
-    })
-
-    it('Login_Test_Correctly',function(){
+    it('Login_Test_Correctly', function () {
         cy.get('#email').type(this.loginData.email)
         cy.get('#password').type(this.loginData.password)
         cy.get('.Login-bto').click()
         cy.get('#Login_bto').should('be.visible').contains('LOGOUT')
     })
 
-    it('Sing up Test',() => {
+    it('Login_Test_Failed', () => {
+        cy.get('#email').type(this.loginData.email)
+        cy.get('#password').type(this.loginData.password)
+        cy.get('.Login-bto').click()
+        cy.get('#Login_bto').should('be.visible').contains('LOGOUT')
+    })
+
+    it.only('Login_Test_Correctly_using_fixture_alias', function () {
+        cy.get('@loginData').then( function (user) {
+            cy.get('#email').type(user.email)
+            cy.get('#password').type(user.password)
+        })
+        cy.get('.Login-bto').click()
+        cy.get('#Login_bto').should('be.visible').contains('LOGOUT')
+    })
+
+    it('Sing up Test', () => {
         cy.get('.Signup-bto-page').click()
         const imageUpload = '602006.jpg'
         cy.get('#file').attachFile(imageUpload)
